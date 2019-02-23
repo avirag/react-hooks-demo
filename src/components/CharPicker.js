b0704d9b6e805cef4dba38c84547a6c37aa33f02
Anita Virag <ik.virag@gmail.com>
2019-02-23 23:20:47 +0100

    custom hooks

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,43 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useHttp } from '../hooks/http';
 
 import './CharPicker.css';
 
 const CharPicker = props => {
-  const [loadedChars, setLoadedChars] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, fetchedData] = useHttp('https://swapi.co/api/people', []);
 
-  // ~ componentDidMount
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://swapi.co/api/people')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch.');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const selectedCharacters = charData.results.slice(0, 5);
-        setIsLoading(false);
-        setLoadedChars(
-          selectedCharacters.map((char, index) => ({
-            name: char.name,
-            id: index + 1
-          }))
-        );
-      })
-      .catch(err => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }, []);
+  const selectedCharacters = fetchedData ? fetchedData.results.slice(0, 5).map((char, index) => ({
+    name: char.name,
+    id: index + 1
+  })) : [];
 
   let content = <p>Loading characters...</p>;
 
   if (
     !isLoading &&
-    loadedChars &&
-    loadedChars.length > 0
+    selectedCharacters &&
+    selectedCharacters.length > 0
   ) {
     content = (
       <select
@@ -45,7 +24,7 @@ const CharPicker = props => {
         value={props.selectedChar}
         className={props.side}
       >
-        {loadedChars.map(char => (
+        {selectedCharacters.map(char => (
           <option key={char.id} value={char.id}>
             {char.name}
           </option>
@@ -54,7 +33,7 @@ const CharPicker = props => {
     );
   } else if (
     !isLoading &&
-    (!loadedChars || loadedChars.length === 0)
+    (!selectedCharacters || selectedCharacters.length === 0)
   ) {
     content = <p>Could not fetch any data.</p>;
   }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,76 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Summary from './Summary';
+import { useHttp } from '../hooks/http'
 
 const Character = props =>  {
-  const [loadedCharacter, setLoadedCharacter] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, fetchedData] = useHttp('https://swapi.co/api/people/' + props.selectedChar, [props.selectedChar]);
 
-  console.log('Rendering...');
-
-  const fetchData = () => {
-    console.log(
-      'Sending Http request for new character with id ' +
-        props.selectedChar
-    );
-
-    setIsLoading(true);
-
-    fetch('https://swapi.co/api/people/' + props.selectedChar)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not fetch person!');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const loadedCharacter = {
-          id: props.selectedChar,
-          name: charData.name,
-          height: charData.height,
-          colors: {
-            hair: charData.hair_color,
-            skin: charData.skin_color
-          },
-          gender: charData.gender,
-          movieCount: charData.films.length
-        };
-        setIsLoading(false);
-        setLoadedCharacter(loadedCharacter);
-      })
-      .catch(err => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  };
-
-  // componentDidMount ~ useEffect with [] dependency
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // componentDidUpdate ~ useEffect if props.selectedChar has changed
-  useEffect(() => {
-    console.log('Fetching data... <-- componentDidUpdate');
-
-    fetchData();
-
-    // componentWillUnmount ~ run just before next change
-    return () => {
-      console.log("Cleaning up... <-- componentWillUnmount");
+  let loadedCharacter = null;
+  if(fetchedData) {
+    loadedCharacter = {
+      id: props.selectedChar,
+      name: fetchedData.name,
+      height: fetchedData.height,
+      colors: {
+        hair: fetchedData.hair_color,
+        skin: fetchedData.skin_color
+      },
+      gender: fetchedData.gender,
+      movieCount: fetchedData.films.length
     };
-  }, [props.selectedChar]);
-
-  // componentDidUnmount
-  useEffect(() => {
-    return () => {
-      console.log('<-- componentDidUnmount');
-    };
-  }, []);
+  }
 
   let content = <p>Loading Character...</p>;
 
-  if (!isLoading && loadedCharacter.id) {
+  if (!isLoading && loadedCharacter) {
     content = (
       <Summary
         name={loadedCharacter.name}
@@ -81,15 +34,11 @@ const Character = props =>  {
         movieCount={loadedCharacter.movieCount}
       />
     );
-  } else if (!isLoading && !loadedCharacter.id) {
+  } else if (!isLoading && !loadedCharacter) {
     content = <p>Failed to fetch character.</p>;
   }
   return content;
 }
 
-// ~ shouldComponentUpdate
-export default React.memo(Character, (prevProps, nextProps) => {
-  return (
-    nextProps.selectedChar !== prevProps.selectedChar // not necessary
-  );
-});
+export default Character;
+
